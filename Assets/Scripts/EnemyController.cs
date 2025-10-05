d7d7edc562e71f177235dd7dc1c8949965e59fb5
KronoEE <juanibernardi019@gmail.com>
2025-10-05 20:35:27 -0300

    Upload recent changes
    -Different character
    -Setting enemy behavior

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -2,15 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class EnemyController : MonoBehaviour
 {
-    void Start()
+    [SerializeField] private Transform player;
+    [SerializeField] private float detectionRadius = 5f;
+    [SerializeField] private float speed = 2f;
+
+    private Rigidbody2D rb;
+    private Vector2 movement;
+    private void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();   
     }
 
-    void Update()
+    private void Update()
     {
-        
+        float distanceToPlayer = Vector2.Distance(transform.position, player.position);
+
+        if (distanceToPlayer < detectionRadius)
+        {
+            Vector2 direction = (player.position - transform.position).normalized;
+
+            movement = new Vector2(direction.x, 0);
+        }
+        else
+        {
+            movement = Vector2.zero;
+        }
+        rb.MovePosition(rb.position + movement * speed * Time.deltaTime);
     }
 }
